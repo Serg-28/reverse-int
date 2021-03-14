902eb28fc6feb258d22794ec4a7be00d6f68c6de
Serg-28 <sergbondarenko28@gmail.com>
2021-03-15 00:19:23 +0200

    feat: add  reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let x = (
+        parseFloat(
+            n
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+        ) * Math.sign(n)
+    );
+    return Math.abs(x);
 }
